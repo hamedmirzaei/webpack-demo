fb50845425e06381838e938050102207670e63cb
Hamed Mirzaei <mirzaeihamed1368@gmail.com>
2019-01-07 10:01:27 +0330

    softlink for node_modules

--- dist/chunk.2.js
+++ dist/chunk.2.js
@@ -1,3 +1,3 @@
-/*! 1ee39c6 */
+/*! 7d44eaf */
 (window.webpackJsonp=window.webpackJsonp||[]).push([[2],{292:function(o,n,w){"use strict";w.r(n),n.default="Hello from lazy"}}]);
 //# sourceMappingURL=chunk.2.js.map
